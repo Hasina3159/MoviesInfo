ac2669e8112a46769c191ca5cc7e7dcb028504a2
Hasina3159 <dev.junior3159@gmail.com>
2024-04-14 21:13:25 +0300

    some changes and amelioration

--- js/script.js
+++ js/script.js
@@ -2,55 +2,79 @@ const title = document.getElementById('title');
 const search = document.getElementById('search');
 const allMovies = document.getElementById('allMovies');
 const pagesDOM = document.getElementById('pages');
-let allPagesNumber;
-let elements = document.getElementsByClassName("element");
+const API_KEY = "aca58a81";
+let elements = document.getElementsByClassName("front");
 let all = {};
-let page = 6;
+let page = 1;
 let pages = 0;
+let actualTitle = "all";
 
 function pageNumber(number){
     pagesDOM.innerHTML = pagesDOM.innerHTML + "\n" + `
-    <div class="number${number != 0 ? "" : " dot"}" id="${number}">
+    <div class="number ${number != 0 ? "" : "dot"} ${number == page ? "actual" : ""}" id="${number}">
         ${number != 0 ? number : "..."}
     </div>    `
 }
 
 function createAllPageNumber(allPages, actualPage){
+    pagesDOM.innerHTML = '';
     if (actualPage > 5)
         pageNumber(0);
-    for(let i = actualPage - 4; i < actualPage; i++){
-        if (i > 0)
+    for(let i = actualPage - 4; i <= parseInt(actualPage) + 4; i++){
+        if (i > 0 && i <= allPages){
             pageNumber(i);
-    }
-    for(let i = actualPage; i <= actualPage + 4 && i <= allPages; i++){
-        pageNumber(i);
+        }
     }
     if (actualPage < allPages - 5)
         pageNumber(0);
 }
 
+function listenPagesNumber(allPagesNumber){
+    for(let i = 0; i < allPagesNumber.length; i++) {
+        if (!allPagesNumber[i].classList.contains('dot'))
+            allPagesNumber[i].addEventListener("click", (e) =>{
+                allMovies.innerHTML = "";
+                page = parseInt(allPagesNumber[i].innerText);
+                searchMovies(actualTitle, (page - 1) * 2 + 1);
+                searchMovies(actualTitle, (page - 1) * 2 + 2);
+            });
+    };
+}
+
 async function getMovieByTitle(title, actualPage){
-    const query = "http://www.omdbapi.com/?s=" + title + "&page=" + actualPage + "&apikey=aca58a81"
+    const loadingIndicator = document.getElementById('loadingIndicator');
+    loadingIndicator.style.display = 'block';
+    const query = `http://www.omdbapi.com/?s=${title}&page=${actualPage}&apikey=${API_KEY}`
     const response = await fetch(query);
     const movies = await response.json();
+    loadingIndicator.style.display = 'none';
     if(movies.Response == "True"){
-        allMovies.innerHTML = "";
-        pages = Math.ceil(parseInt(movies.totalResults) / 10);
-        alert("Movies : " + movies.totalResults + "; Pages : " + pages);
+        pages = Math.floor(Math.ceil(parseInt(movies.totalResults) / 10) / 2);
         movies.Search.forEach(infos => {
             showMovie(infos);
         });
-        createAllPageNumber(pages, actualPage);
-        allPagesNumber = document.getElementsByClassName("number");
+		let actual = page;
+        createAllPageNumber(pages, actual);
+        actualTitle = title;
+        let allPagesNumber = document.getElementsByClassName("number");
+        listenPagesNumber(allPagesNumber);
+
     }else{
-        allMovies.innerHTML = "<div class='notFound'> Movie not found!</div>";
+        allMovies.innerHTML = `<div class='notFound'> "${title}" Movie not found!</div>`;
     }
 };
 
+/*async function getMovie(title, page){
+    
+}*/
+
 async function getMovieByID(ID){
-    const query = "http://www.omdbapi.com/?i=" + ID + "&apikey=aca58a81"
+    const loadingIndicator = document.getElementById('loadingIndicator');
+    loadingIndicator.style.display = 'block';
+    const query = `http://www.omdbapi.com/?i=${ID}&apikey=${API_KEY}`;
     const response = await fetch(query);
     const movies = await response.json();
+    loadingIndicator.style.display = 'none';
     showMovieDetails(movies);
 };
 
@@ -81,8 +105,9 @@ function showMovieDetails(infos){
 
 function showMovie(infos){
     allMovies.innerHTML = allMovies.innerHTML + `
-        <div class="element" id="${infos.imdbID}">
+        <div class="element">
             <div class="image">
+				<div class="front" id="${infos.imdbID}"></div>
                 <img src="${infos.Poster}" alt="">
             </div>
             <div class="infos">
@@ -97,12 +122,11 @@ function showMovie(infos){
     `
 }
 
-async function searchMovies(movieTitle){
-    getMovieByTitle(movieTitle, page).then(() => {
-        elements = document.getElementsByClassName("element");
+async function searchMovies(movieTitle, actualPage){
+    getMovieByTitle(movieTitle, actualPage).then(() => {
+        elements = document.getElementsByClassName("front");
         for(let i = 0; i < elements.length; i++) {
             elements[i].addEventListener("click", (e) =>{
-                console.log(e.currentTarget.id);
                 getMovieByID(e.currentTarget.id);
             });
         };
@@ -110,11 +134,19 @@ async function searchMovies(movieTitle){
 }
 
 search.addEventListener("click", (e) => {
-    searchMovies(title.value);
+    allMovies.innerHTML = "";
+    page = 1;
+    searchMovies(title.value, (page - 1) * 2 + 1);
+    searchMovies(title.value, (page - 1) * 2 + 2);
 });
 
 title.addEventListener("keypress", (e) => {
-    if(e.key == "Enter")
-        searchMovies(title.value);
+    if(e.key == "Enter"){
+		allMovies.innerHTML = "";
+		page = 1;
+		searchMovies(title.value, (page - 1) * 2 + 1);
+		searchMovies(title.value, (page - 1) * 2 + 2);
+	}
 });
-searchMovies("all");
+searchMovies("all", 1);
+searchMovies("all", 2);
